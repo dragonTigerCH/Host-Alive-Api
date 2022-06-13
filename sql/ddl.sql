b6347723f196244031ff2a25f60661d2034d4250
dragonTigerCH <yoho9908@gmail.com>
2022-06-13 23:14:14 +0900

    sql 수정

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,7 +3,9 @@ create table Host (
        id bigint not null auto_increment,
         createdDate datetime(6),
         lastModifiedDate datetime(6),
+        createdDateAlive datetime(6),
         ip varchar(255),
+        isAlive bit not null,
         name varchar(255),
         primary key (id)
- );
+    );
